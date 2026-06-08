e2de1f99b3906ba2301be3dd2f550061ad0795fa
nina.homeniuk <nina.homeniuk.work@gmail.com>
2026-06-09 00:12:05 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from website.generator import *
 from filemanager import *
+import sys
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -7,9 +8,12 @@ dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    base_path = "/" if sys.argv[0] is None else sys.argv[0]
+    print(base_path)
     clean_folder(dir_path_public)
     copy_files(dir_path_static, dir_path_public)
     generate_pages_recursive(
+        base_path,
         dir_path_content,
         template_path,
         dir_path_public,
--- src/website/generator.py
+++ src/website/generator.py
@@ -1,9 +1,11 @@
 import os
 from parsers.parser import *
 
-def generate_pages_recursive(sourse: str, 
-                             template_path: str, 
-                             destination: str) -> None:
+def generate_pages_recursive(
+        base_path: str,
+        sourse: str,
+        template_path: str,
+        destination: str) -> None:
     for item in os.listdir(sourse):
         sourse_path = os.path.join(sourse, item)
         dest_path = os.path.join(destination, item)
@@ -12,17 +14,19 @@ def generate_pages_recursive(sourse: str,
                 and sourse_path.endswith(".md")):
                 dest_path = dest_path.rstrip(".md") + ".html"
                 print(f"{sourse_path} -> {dest_path}")
-                generate_page(sourse_path, template_path, dest_path)
+                generate_page(base_path, sourse_path, template_path, dest_path)
             elif os.path.isdir(sourse_path):
                 print(f"{sourse_path} -> {dest_path}")
                 os.mkdir(dest_path)
-                generate_pages_recursive(sourse_path, template_path, dest_path)
+                generate_pages_recursive(base_path, sourse_path, template_path, dest_path)
         except Exception as e:
             print(f"Failed to generate {sourse_path} to {dest_path}. Reason: {e}")
 
-def generate_page(from_path: str, 
-                  template_path: str, 
-                  dest_path: str) -> None:
+def generate_page(
+        base_path: str,
+        from_path: str,
+        template_path: str,
+        dest_path: str) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     markdown = open(from_path).read()
@@ -30,7 +34,11 @@ def generate_page(from_path: str,
     content = markdown_to_html_node(markdown).to_html()
     
     template = open(template_path).read()
-    full_page = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    full_page = template\
+    .replace("{{ Title }}", title)\
+    .replace("{{ Content }}", content)\
+    .replace("href=\"/", f"href=\"{base_path}")\
+    .replace("src=\"/", f"src=\"{base_path}")
     
     with open(dest_path, "w") as page:
         page.write(full_page)
