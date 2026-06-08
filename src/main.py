572d911673c1da392a06b0b43290c6185866faf4
nina.homeniuk <nina.homeniuk.work@gmail.com>
2026-06-09 00:12:29 +0200

    update

--- src/main.py
+++ src/main.py
@@ -3,20 +3,20 @@ from filemanager import *
 import sys
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
     base_path = "/" if sys.argv[0] is None else sys.argv[0]
     print(base_path)
-    clean_folder(dir_path_public)
-    copy_files(dir_path_static, dir_path_public)
+    clean_folder(dir_path_docs)
+    copy_files(dir_path_static, dir_path_docs)
     generate_pages_recursive(
         base_path,
         dir_path_content,
         template_path,
-        dir_path_public,
+        dir_path_docs,
     )
     
 main()
